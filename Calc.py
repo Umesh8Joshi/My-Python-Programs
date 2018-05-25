c4865091108578d3fe342539dfd3c9d6faccef52
Umesh Joshi <umesh14el007@gmail.com>
2018-05-25 23:48:44 +0530

    Calc file

---
+++ Calc.py
@@ -0,0 +1,17 @@
+'''
+The following class will be used as the calculator
+Class will have different methods to perform simple expressions
+'''
+
+class Calc():
+	def __init__(a,b,self):
+		self.a = a
+		self.b = b
+	def sum(a,b,self):
+		return self.a + self.b
+	def min(a,b,self):
+		return self.a - self.b
+	def mul(a,b,self):
+		return self.a*self.b
+	def div(a,b,self):
+		return self.a / self.b
